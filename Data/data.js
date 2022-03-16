0f9b78685c419d6a80fe92db176c048fb4749e57
Sarushan Thiyakumar <sarushan.thiy@gmail.com>
2022-03-16 16:44:07 +0000

    made more data

---
+++ Data/data.js
@@ -0,0 +1,54 @@
+const results = [
+    {
+      id: 1,
+      name: "Health Benefits of Cucumber",
+      link: "https://www.medicalnewstoday.com/articles/283006",
+    },
+    {
+      id: 2,
+      name: "Vegetables",
+      link:"https://en.wikipedia.org/wiki/Vegetable" ,
+    },
+    {
+      id: 3,
+      name: "The 14 Most Nutrient-Dense Vegetables",
+      link: "https://www.healthline.com/nutrition/14-healthiest-vegetables-on-earth",
+    },
+    {
+      id: 4,
+      name: "Is a Tomato a Fruit or a Vegetable?",
+      link: "https://www.britannica.com/story/is-a-tomato-a-fruit-or-a-vegetable",
+    },
+    {
+      id: 5,
+      name: "Is the Mushroom a Vegetable?",
+      link: "https://www.bestfoodfacts.org/is-the-mushroom-a-vegetable-2/",
+    },
+    {
+      id: 6,
+      name: "Meat",
+      link: "https://en.wikipedia.org/wiki/Meat",
+    },
+    {
+      id: 7,
+      name: "How to Cook Steak – like a chef!",
+      link: "https://www.recipetineats.com/how-to-cook-steak/",
+    },
+    {
+      id: 8,
+      name: "Steak",
+      link: "https://en.wikipedia.org/wiki/Steak",
+    },
+    {
+      id: 9,
+      name: "Meat definition",
+      link: "https://www.merriam-webster.com/dictionary/meat",
+    },
+    {
+      id: 10,
+      name: "Meat Britannica",
+      link: "https://www.britannica.com/topic/meat",
+    },
+  ];
+
+  module.exports = results
